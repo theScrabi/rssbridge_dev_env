15ae7e0f2aad714f048708b479e044790e0caccb
Christian Schabesberger <chris.schabesberger@mailbox.org>
2022-08-13 13:24:45 +0200

    modernize helper scripts

--- clear.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker exec -it rssbridge /bin/bash -c "rm -r /app/cache/*"
-docker exec -it rssbridge /bin/bash -c "pkill apache"
-sleep 2
-docker rm rssbridge
-./run.sh
--- stop.sh
+++ stop.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+docker exec -it rssbridge /bin/bash -c 'kill $(for I in $(ls /proc | grep "[0-9]"); do echo -n "$I "; cat /proc/$I/comm; done 2> /dev/null | grep php | grep "[0-9]") 2> /dev/null'
 docker stop rssbridge
 docker rm rssbridge
