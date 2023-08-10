e94aa1cb13d1ea5fd98a73efc86da65c0244b2a3
Schabi <chris.schabesberger@mailbox.org>
2023-08-10 13:36:23 +0200

    clear cache

--- stop.sh
+++ stop.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-docker exec -it rssbridge /bin/bash -c 'kill $(for I in $(ls /proc | grep "[0-9]"); do echo -n "$I "; cat /proc/$I/comm; done 2> /dev/null | grep php | grep "[0-9]") 2> /dev/null'
+docker exec -it rssbridge /bin/bash -c 'rm -fr /app/cache/* && kill $(for I in $(ls /proc | grep "[0-9]"); do echo -n "$I "; cat /proc/$I/comm; done 2> /dev/null | grep php | grep "[0-9]") 2> /dev/null'
+
 docker stop rssbridge
 docker rm rssbridge
