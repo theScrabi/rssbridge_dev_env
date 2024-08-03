ab501ffd6edc9a4f8410b3f8aee448d468def6ba
Christian Schabesberger <chris.schabesberger@mailbox.org>
2024-08-03 20:54:07 +0200

    add test for nn

--- test.sh
+++ test.sh
@@ -28,6 +28,30 @@ EOM
 
 for I in $REGIONS
 do
-    curl "http://localhost:8081/?action=display&bridge=Nordbayern&region=$I&format=Html" \
+    #curl "http://localhost:8081/?action=display&bridge=Nordbayern&region=$I&format=Html" \
+    #    | grep "logo"
+	echo
+done
+
+REGIONS=$(cat << EOM
+ansbach
+erlangen
+erlangen-hoechstadt
+forchheim
+fuerth
+gunzenhausen
+neumarkt
+neustadt-aisch-bad-windsheim
+nuernberg
+nuernberger-land
+regensburg
+schwabach
+weissenburg
+EOM
+)
+
+for I in $REGIONS
+do
+    curl "http://localhost:8081/?action=display&bridge=NurembergerNachrichten&region=$I&format=Html" \
         | grep "logo"
 done
